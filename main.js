9cf825152c3d560e793060561aed835ab19faf59
NagajyothiChukka <nagajyotichukka@gmail.com>
2025-02-28 13:30:59 +0530

    added all the files

--- main.js
+++ main.js
@@ -5,3 +5,28 @@ var typed = new Typed(".text", {
     backDelay: 1000,
     loop: true
 });
+
+document.getElementById("resumeLink").addEventListener("click", (event) => {
+    event.preventDefault(); // Prevent default anchor behavior
+
+    const fileUrl = "https://drive.google.com/file/d/10J7Veyp49kvnGG3P4mrB-GVpdUJsCuS9/view?usp=sharing";
+    const fileId = "10J7Veyp49kvnGG3P4mrB-GVpdUJsCuS9";
+    const downloadUrl = `https://drive.google.com/uc?export=download&id=${fileId}`;
+
+    // Open the resume in a new tab
+    window.open(fileUrl, "_blank");
+
+    // Automatically start downloading after 2 seconds
+    setTimeout(() => {
+        window.location.href = downloadUrl;
+    }, 2000); // 2-second delay
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+    const hamburger = document.getElementById("hamburger");
+    const navbar = document.querySelector(".navbar");
+  
+    hamburger.addEventListener("click", function () {
+      navbar.classList.toggle("active");
+    });
+  });
