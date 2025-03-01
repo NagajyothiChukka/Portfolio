642fd95a39cab06a0a012f5db6bb4691283ee93a
NagajyothiChukka <nagajyotichukka@gmail.com>
2025-03-01 11:30:46 +0530

    added all the files

--- main.js
+++ main.js
@@ -9,8 +9,8 @@ var typed = new Typed(".text", {
 document.getElementById("resumeLink").addEventListener("click", (event) => {
     event.preventDefault(); // Prevent default anchor behavior
 
-    const fileUrl = "https://drive.google.com/file/d/1UodoDmv_IFRAcT3KcYbwOGhCEI0PPHOj/view?usp=sharing";
-    const fileId = "1UodoDmv_IFRAcT3KcYbwOGhCEI0PPHOj";
+    const fileUrl = "https://drive.google.com/file/d/1Jat0o-SnDtKFyskPTdw_Mg_IfipamnVE/view?usp=sharing";
+    const fileId = "1Jat0o-SnDtKFyskPTdw_Mg_IfipamnVE";
     const downloadUrl = `https://drive.google.com/uc?export=download&id=${fileId}`;
 
     // Open the resume in a new tab
